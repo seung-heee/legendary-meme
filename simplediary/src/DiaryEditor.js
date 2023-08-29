5e97bba8aca55b26e622a923382d8521c46e0910
seung-heee <tmdgmldi2595@naver.com>
2023-08-30 00:22:47 +0900

    useRef로 DOM 조작, focus

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () => {
-    // 동작이 비슷한 State 묶기
+    // useRef : DOM 조작
+    const authorInput = useRef();
+    const contentInput = useRef();
+
+    // useState : 동작이 비슷한 State 묶기, state 객체 형태로
     const [state, setState] = useState({
         author: "",
         content: "",
@@ -9,17 +13,27 @@ const DiaryEditor = () => {
     })
 
     const handleChangeState = (e) => {
-        console.log(e.target.name);
-        console.log(e.target.value);
         setState({
             ...state,
+            // 변경된 key : value
             [e.target.name]: e.target.value,
         })
     }
 
     const handleSubmit = (e) => {
-        console.log(state);
-        alert('저장 성공~');
+        if (state.author.length < 1) {
+            // alert은 트렌디 하지 않다 ~
+            // alert("작성자 최소 1글자 이상 입력");
+            authorInput.current.focus(); // useRef로 focus 기능 사용
+            return;
+        }
+        if (state.content.length < 5) {
+            // alert("본문 최소 5글자 이상 입력");
+            contentInput.current.focus();
+            return;
+        }
+
+        alert('저장 성공!');
     }
 
     return (
@@ -27,6 +41,7 @@ const DiaryEditor = () => {
             <h3>오늘의 일기</h3>
             <div>
                 <input
+                    ref={authorInput}
                     name="author"
                     value={state.author}
                     onChange={handleChangeState}
@@ -34,6 +49,7 @@ const DiaryEditor = () => {
             </div>
             <div>
                 <textarea
+                    ref={contentInput}
                     name="content"
                     value={state.content}
                     onChange={handleChangeState}
@@ -49,7 +65,7 @@ const DiaryEditor = () => {
                 </select>
             </div>
             <div>
-                <button onClick={handleSubmit}>일기 저장하기!</button>
+                <button onClick={handleSubmit}>일기 저장하기</button>
             </div>
         </div>
     )
