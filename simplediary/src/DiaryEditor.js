07247ed08b3602d8233041fa45acd5da11fc118b
seung-heee <tmdgmldi2595@naver.com>
2023-08-30 00:10:16 +0900

    동작 비슷한 state 묶기

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -1,8 +1,26 @@
 import { useState } from "react";
 
 const DiaryEditor = () => {
-    const [author, setAuthor] = useState("");
-    const [content, setContent] = useState("");
+    // 동작이 비슷한 State 묶기
+    const [state, setState] = useState({
+        author: "",
+        content: "",
+        emotion: 1,
+    })
+
+    const handleChangeState = (e) => {
+        console.log(e.target.name);
+        console.log(e.target.value);
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = (e) => {
+        console.log(state);
+        alert('저장 성공~');
+    }
 
     return (
         <div className="DiaryEditor">
@@ -10,16 +28,28 @@ const DiaryEditor = () => {
             <div>
                 <input
                     name="author"
-                    value={author}
-                    onChange={(e) => { setAuthor(e.target.value) }}
+                    value={state.author}
+                    onChange={handleChangeState}
                 />
             </div>
             <div>
                 <textarea
                     name="content"
-                    value={content}
-                    onChange={(e) => { setContent(e.target.value) }}
+                    value={state.content}
+                    onChange={handleChangeState}
                 />
+            </div>
+            <div>
+                <select name="emotion" value={state.emotion} onChange={handleChangeState}>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                </select>
+            </div>
+            <div>
+                <button onClick={handleSubmit}>일기 저장하기!</button>
             </div>
         </div>
     )
