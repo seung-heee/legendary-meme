0bbedf155761d715a60a09a9e5e894799bcf0921
seung-heee <tmdgmldi2595@naver.com>
2023-08-30 13:56:06 +0900

    useEffect, toggle로 LifeCycle 제어

--- simplediary/src/LifeCycle.js
+++ simplediary/src/LifeCycle.js
@@ -1,38 +1,28 @@
 import React, { useEffect, useState } from 'react';
 
-const LifeCycle = () => {
-    const [count, setCount] = useState(0);
-    const [text, setText] = useState("");
-
+const UnmountTest = () => {
     useEffect(() => {
-        console.log('Mount');
+        console.log('Mount!');
+
+        return () => {
+            // Unmount 시점에 실행
+            console.log("UnMount!");
+        };
     }, []);
 
-    useEffect(() => {
-        console.log('Update!');
-    });
+    return (
+        <div>Unmount testing Component</div>
+    )
+}
 
-    useEffect(() => {
-        console.log('Count 업데이트');
-        if (count > 5) {
-            alert('count가 5를 넘었습니다. 1로 초기화합니다.');
-            setCount(1);
-        }
-    }, [count]);
-
-    useEffect(() => {
-        console.log('text 업데이트');
-    }, [text]);
+const LifeCycle = () => {
+    const [isVisible, setisVisible] = useState(false);
+    const toggle = () => setisVisible(!isVisible); // boolean toggle
 
     return (
         <div style={{ padding: 20 }} className="LifeCycle">
-            <div>
-                {count}
-                <button onClick={() => setCount(count + 1)}>+</button>
-            </div>
-            <div>
-                <input value={text} onChange={(e) => setText(e.target.value)} />
-            </div>
+            <button onClick={toggle}>ON/OFF</button>
+            {isVisible && <UnmountTest />}
         </div>
     )
 }
