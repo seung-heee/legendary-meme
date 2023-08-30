858334893b4c3f8d1ed722b2c3f1797859ee9855
seung-heee <tmdgmldi2595@naver.com>
2023-08-30 23:07:41 +0900

    React.memo 컴포넌트 재사용(객체)

--- simplediary/src/Optimizetest.js
+++ simplediary/src/Optimizetest.js
@@ -1,38 +1,50 @@
 import React, { useState, useEffect } from "react";
 
-const TextView = React.memo(({ text }) => {
+const CounterA = React.memo(({ count }) => {
     useEffect(() => {
-        console.log(`update : text : ${text}`);
-    })
-    return <div>{text}</div>
-})
-
-const CountView = React.memo(({ count }) => {
-    useEffect(() => {
-        console.log(`update : count : ${count}`);
+        console.log('A', count);
     })
     return <div>{count}</div>
 })
 
+// 객체에 담긴 값은 얕은 비교(주소)를 하기 때문에 값이 변하지 않아도 리렌더링 발생
+// 객체의 주소에 대한 비교
+const CounterB = ({ obj }) => {
+    useEffect(() => {
+        console.log('B', obj.count);
+    })
+    return <div>{obj.count}</div>
+}
+
+// React.memo의 비교 함수로 작용, 값 비교로 판단
+const areEqual = (prevProps, nextProps) => {
+    return prevProps.obj.count === nextProps.obj.count;
+} // true : reRendering X
+
+
+// CounterB는 areEqual 함수의 판단에 따라 reRendering 여부 결정됨.
+const MemoizedCounterB = React.memo(CounterB, areEqual);
+
 const Optimizetest = () => {
     const [count, setCount] = useState(1);
-    const [text, setText] = useState("");
+    const [obj, setObj] = useState({
+        count: 1
+    })
 
     return (
         <div style={{ padding: 50 }}>
             <div>
-                <h2>count</h2>
-                <CountView count={count} />
-                <button onClick={() => setCount(count + 1)}>+</button>
+                <h2>Counter A</h2>
+                <CounterA count={count} />
+                <button onClick={() => setCount(count)}>A button</button>
             </div>
-
             <div>
-                <h2>text</h2>
-                <TextView text={text} />
-                <input value={text} onChange={(e) => setText(e.target.value)} />
+                <h2>Counter B</h2>
+                <MemoizedCounterB obj={obj} />
+                <button onClick={() => setObj({ count: obj.count })}>B button</button>
             </div>
-        </div>
+        </div >
     )
 }
 
-export default Optimizetest; 
+export default Optimizetest;
