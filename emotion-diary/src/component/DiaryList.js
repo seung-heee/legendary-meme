33d73ecbff0f4a7180a5a6e2679a9eb8a17c5f16
seung-heee <tmdgmldi2595@naver.com>
2023-09-03 22:49:55 +0900

    sort -good, bad emotion

--- emotion-diary/src/component/DiaryList.js
+++ emotion-diary/src/component/DiaryList.js
@@ -1,8 +1,15 @@
 import { useState } from "react"
+import MyButton from "./MyButton"
 
 const sortOptionList = [
     { value: 'latest', name: '최신순' },
     { value: 'oldest', name: '오래된순' },
+]
+
+const filterOptionList = [
+    { value: 'all', name: '전부 다' },
+    { value: 'good', name: '좋은 감정만' },
+    { value: 'bad', name: '안좋은 감정만' },
 ]
 
 const ControlMenu = ({ value, onChange, optionList }) => {
@@ -13,9 +20,17 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
     const [sortType, setSortType] = useState('lastest');
+    const [filter, setFilter] = useState('all');
 
-    // 최신순, 오래된순 정렬
     const getProcessedDiaryList = () => {
+        const filterCallBack = (item) => {
+            if (filter === 'good') {
+                return parseInt(item.emotion) <= 3;
+            } else {
+                return parseInt(item.emotion) > 3;
+            }
+        }
+
         const compare = (a, b) => {
             if (sortType === 'latest') {
                 return parseInt(b.date) - parseInt(a.date);
@@ -24,15 +39,17 @@ const DiaryList = ({ diaryList }) => {
             }
         }
         const copyList = JSON.parse(JSON.stringify(diaryList));
-        const sortedList = copyList.sort(compare);
+        const filterdList = filter === 'all' ? copyList : copyList.filter((it) => filterCallBack(it));
+        const sortedList = filterdList.sort(compare);
         return sortedList;
     }
 
     return <div>
         <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList} />
-
+        <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList} />
+        <MyButton text={'새 일기쓰기'} onClick={() => { }} type={'positive'} />
         {getProcessedDiaryList().map((it) =>
-            <div key={it.id}>{it.content}</div>
+            <div key={it.id}>{it.content}, {it.emotion}점</div>
         )}
     </div>
 }
